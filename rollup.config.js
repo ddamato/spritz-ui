cc06d1b5d78f18bbf15ae2e11bd7180d133309a6
Donnie D'Amato <fauxserious@gmail.com>
2021-06-04 18:06:56 -0400

    init project

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import pkg from './package.json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: './index.js',
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
+  output: [{
+    file: pkg.main,
+    exports: 'named',
+    format: 'umd',
+    name: 'spritz-ui',
+  }, {
+    file: pkg.module,
+    format: 'esm',
+  }]
+};
